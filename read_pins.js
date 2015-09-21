f6c2287c4146453b8a36e2450dc756a240c1e216
Austin Mullins <austin.mullins@gmail.com>
2015-09-21 02:35:32 +0000

    Correct read_pins and test in console.

--- read_pins.js
+++ read_pins.js
@@ -8,46 +8,43 @@ const ON = 1;
 
 wpi.setup("wpi");
 
-var PinReader = function(pinList) {
+var PinReader = function(pinList, listener) {
   this.pins = pinList;
   this._emitter = new EventEmitter();
   this.pinStates = {};
   this.pins.forEach(function(pin) {
     this.pinStates[pin] = UNOPENED;
   }.bind(this));
+
+  if (typeof(listener) == "function") {
+    this.addPinChangeListener(listener);
+    this.openPins();
+  }
 };
 
 PinReader.prototype.addPinChangeListener = function(listener) {
   this._emitter.addListener("pinChanged", listener);
 };
 
-var eachPin = function(pins, callback) {
-  var current = Promise.resolve();
-  return Promise.map(pins, function(pin) {
-    current = current.then(function() {
-      return callback(pin);
-    });
-    return current;
-  });
-};
-
 PinReader.prototype._watchPin = function(pin) {
   var _self = this;
   if (_self.pinStates[pin] != UNOPENED) {
-   var value = wpi.readDigital(pin);
-   if (value != _self.pinStates[pin]) {
+    var value = wpi.digitalRead(pin);
+    if (value != _self.pinStates[pin]) {
        _self.pinStates[pin] = value;
        _self._emitter.emit("pinChanged", {pin: pin, value: value});
-   }
+    }
 
-   setTimeout(function() {
-     _self._watchPin(pin);
-   }, 20);
+    setTimeout(function() {
+      _self._watchPin(pin);
+    }, 20);
+  }
 };
 
 PinReader.prototype.openPins = function() {
   this.pins.map(function(pin) {
-    wpi.pinMode(pin, wpi.modes.INPUT);
+    wpi.pinMode(pin, wpi.INPUT);
+    wpi.pullUpDnControl(pin, wpi.PUD_UP);
     this.pinStates[pin] = UNREAD;
     setImmediate(function() {
       this._watchPin(pin);
