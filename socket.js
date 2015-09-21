8d19a9295e3c5efce98f424c80264ed609dfe560
Austin Mullins <austin.mullins@gmail.com>
2015-09-21 17:26:34 +0000

    Add socket.js

---
+++ socket.js
@@ -0,0 +1,46 @@
+var TiebreakerStatus = require("./status");
+var dgram = require("dgram");
+
+var TiebreakerSocket = function(port) {
+	this.port = port;
+	this._socket = dgram.createSocket("udp4");
+	this._socket.bind(port, this._handleMessage.bind(this));
+	this._status = new TiebreakerStatus();
+};
+
+TiebreakerSocket.prototype._handleMessage = function(msg, rinfo) {
+	if (msg) {
+		var text = msg.toString();
+		switch (text) {
+			case "QRY": this._sendStatus(rinfo.address); break;
+			case "RST": this._resetStatus(rinfo.address); break;
+			default: this._sendError(rinfo.adrress); break;
+		}
+	}
+};
+
+TiebreakerSocket.prototype._sendStatus = function(address) {
+	this._sendMessage(JSON.stringify(this._status), address);
+};
+
+TiebreakerSocket.prototype._resetStatus = function(address) {
+	this._status.reset();
+	this._socket.sendMessage("OK", address);
+};
+
+TiebreakerSocket.prototype._sendError = function(address) {
+	this._socket.sendMessage("ERROR", address);
+};
+
+TiebreakerSocket.prototype._sendMessage = function(message, address) {
+	var buffer = new Buffer(message);
+	this._socket.send(
+		buffer,
+		0,
+		buffer.data.length,
+		this.port,
+		address);
+};
+
+module.exports = TiebreakerSocket;
+
